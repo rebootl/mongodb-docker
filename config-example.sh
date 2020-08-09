dbe555ac0b9666674dbf2e8c7ad96aa6fe158e1e
Cem Aydin <cem.aydin@gmx.ch>
2020-08-10 00:40:47 +0200

    add PRODUCTION option

--- config-example.sh
+++ config-example.sh
@@ -2,4 +2,7 @@ APP="appname"
 USER="${APP}-admin"
 PASSWORD="example123"
 COLLECTIONS=( "data" "foo" )
+
+PRODUCTION=true
+
 LOCALPORT="27017"
--- run.sh
+++ run.sh
@@ -4,17 +4,27 @@
 . config.sh
 
 # options
-#  -d
-#  -p 27017:27017 \
-#  --network "${APP}-network" \
 #  mongod --replSet rs0
 
-docker run -p "${LOCALPORT}":27017 \
-  -e MONGO_INITDB_ROOT_USERNAME="$USER" \
-  -e MONGO_INITDB_ROOT_PASSWORD="$PASSWORD" \
-  -e MONGO_INITDB_DATABASE="$APP" \
-  -v "$(pwd)/mongodb-data:/data/db" \
-  --restart unless-stopped \
-  --name "mongodb-${APP}" \
-  "mongodb-${APP}" \
-  mongod
+if [ "$PRODUCTION" = true ]; then
+  docker run -d \
+    -e MONGO_INITDB_ROOT_USERNAME="$USER" \
+    -e MONGO_INITDB_ROOT_PASSWORD="$PASSWORD" \
+    -e MONGO_INITDB_DATABASE="$APP" \
+    -v "$(pwd)/mongodb-data:/data/db" \
+    --restart unless-stopped \
+    --name "mongodb-${APP}" \
+    --network "${APP}-network" \
+    "mongodb-${APP}" \
+    mongod
+else
+  docker run -p "${LOCALPORT}":27017 \
+    -e MONGO_INITDB_ROOT_USERNAME="$USER" \
+    -e MONGO_INITDB_ROOT_PASSWORD="$PASSWORD" \
+    -e MONGO_INITDB_DATABASE="$APP" \
+    -v "$(pwd)/mongodb-data:/data/db" \
+    --restart unless-stopped \
+    --name "mongodb-${APP}" \
+    "mongodb-${APP}" \
+    mongod
+fi
